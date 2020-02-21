c4e62c9e41fd174ec584f751e5687c58a67f9bfd
Jakub Vrana <jakub@vrana.cz>
2020-02-21 10:24:18 +0100

    Display cudf

--- dist/plugin.js
+++ dist/plugin.js
@@ -24,5 +24,42 @@ W.loadPlugin(
 '',
 /* Constructor */
 function () {
-  this.onopen = function () {};
+  var store = W.require('store');
+
+  var map = W.require('map');
+
+  var flymet;
+
+  this.onopen = function () {
+    flymet = L.imageOverlay('https://flymet.meteopress.cz/cr/cudf13.png', [[48, 11.65], [51.65, 19.35]], {
+      opacity: .5
+    });
+    updateFlymet();
+    store.on('timestamp', updateFlymet);
+  };
+
+  function updateFlymet() {
+    var timestamp = new Date(store.get('timestamp'));
+    var now = new Date();
+    var hour = timestamp.getUTCHours();
+
+    if (isSameDay(timestamp, now) && hour > 0) {
+      flymet.setUrl('https://flymet.meteopress.cz/cr/cudf' + hour + '.png').addTo(map);
+    } else if (isSameDay(timestamp, addDay(now))) {
+      flymet.setUrl('https://flymet.meteopress.cz/cr' + (hour ? 'dl/cudf' + hour : '/cudf24') + '.png').addTo(map);
+    } else if (isSameDay(timestamp, addDay(now)) && hour == 0) {
+      flymet.setUrl('https://flymet.meteopress.cz/crdl/cudf24.png').addTo(map);
+    } else {
+      flymet.remove();
+    }
+  }
+
+  function addDay(date) {
+    date.setDate(date.getDate() + 1);
+    return date;
+  }
+
+  function isSameDay(a, b) {
+    return a.toISOString().substr(0, 10) == b.toISOString().substr(0, 10);
+  }
 });
