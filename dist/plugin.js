c0089b63f844391feff604ba2d48b2b147907bf9
Jakub Vrana <jakub@vrana.cz>
2020-02-21 11:45:29 +0100

    Add menu

--- dist/plugin.js
+++ dist/plugin.js
@@ -8,20 +8,22 @@ W.loadPlugin(
 /* Mounting options */
 {
   "name": "windy-plugin-flymet",
-  "version": "0.5.0",
+  "version": "0.6.0",
   "author": "Jakub Vrana",
   "repository": {
     "type": "git",
     "url": "git+https://github.com/vrana/windy-plugin-flymet"
   },
   "description": "Windy plugin for Flymet.",
-  "displayName": "My super plugin",
-  "hook": "menu"
+  "displayName": "Flymet",
+  "hook": "menu",
+  "className": "plugin-lhpane plugin-mobile-fullscreen",
+  "exclusive": "lhpane"
 },
 /* HTML */
-'',
+'<div class="plugin-content"> <h2>Flymet</h2> <ul style="padding-left: 20px;"> <li><a data-flymet="">nic</a></li> <li><a data-flymet="cudf" style="font-weight: bold;">Deficit konvektivní teploty</a></li> <li><a data-flymet="cukh">Kupovitá oblačnost</a></li> <li><a data-flymet="cuvr">Rychlost stoupání</a></li> </ul> <p>Neprůhlednost: <input id="flymet-opacity" type="range" min="0" max="100" value="50" style="vertical-align: text-bottom;"></p> <p>Zdroj dat: <a href="https://flymet.meteopress.cz/cr/" target="_blank">Flymet</a></p> </div>',
 /* CSS */
-'',
+'.onwindy-plugin-flymet .left-border{left:400px}.onwindy-plugin-flymet #search{display:none}#windy-plugin-flymet{width:400px;height:100%}#windy-plugin-flymet .plugin-content{padding:20px 15px 15px 15px;font-size:14px;line-height:1.6}#windy-plugin-flymet a{color:navy}',
 /* Constructor */
 function () {
   var store = W.require('store');
@@ -29,6 +31,7 @@ function () {
   var map = W.require('map');
 
   var flymet;
+  var flymetType = 'cudf';
 
   this.onopen = function () {
     flymet = L.imageOverlay('https://flymet.meteopress.cz/cr/cudf13.png', [[48, 11.65], [51.65, 19.35]], {
@@ -43,16 +46,33 @@ function () {
     var now = new Date();
     var hour = timestamp.getUTCHours();
 
-    if (isSameDay(timestamp, now) && hour > 0) {
-      flymet.setUrl('https://flymet.meteopress.cz/cr/cudf' + hour + '.png').addTo(map);
+    if (!flymetType) {
+      flymet.remove();
+    } else if (isSameDay(timestamp, now) && hour > 0) {
+      flymet.setUrl('https://flymet.meteopress.cz/cr/' + flymetType + hour + '.png').addTo(map);
     } else if (isSameDay(timestamp, addDay(now))) {
-      flymet.setUrl('https://flymet.meteopress.cz/cr' + (hour ? 'dl/cudf' + hour : '/cudf24') + '.png').addTo(map);
+      flymet.setUrl('https://flymet.meteopress.cz/cr' + (hour ? 'dl/' + flymetType + hour : '/' + flymetType + '24') + '.png').addTo(map);
     } else if (isSameDay(timestamp, addDay(now)) && hour == 0) {
-      flymet.setUrl('https://flymet.meteopress.cz/crdl/cudf24.png').addTo(map);
+      flymet.setUrl('https://flymet.meteopress.cz/crdl/' + flymetType + '24.png').addTo(map);
     } else {
       flymet.remove();
     }
   }
+
+  function changeFlymet(e) {
+    document.querySelector('[data-flymet="' + flymetType + '"]').style.fontWeight = 'normal';
+    e.target.style.fontWeight = 'bold';
+    flymetType = e.target.dataset['flymet'];
+    updateFlymet();
+  }
+
+  Array.from(document.querySelectorAll('[data-flymet]')).forEach(function (el) {
+    return el.onclick = changeFlymet;
+  });
+
+  document.querySelector('#flymet-opacity').oninput = function (e) {
+    flymet.setOpacity(e.target.value / 100);
+  };
 
   function addDay(date) {
     date.setDate(date.getDate() + 1);
--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // Display name of your plugin, as it will appear in the menu
-    displayName: 'My super plugin',
+    displayName: 'Flymet',
 
     // Place, where opening link to your plugin will appear. So far only
     // Allowed: 'contextmenu', 'menu'
@@ -11,14 +11,14 @@ module.exports = {
     // dependencies: ['https://unpkg.com/d3@5.7.0/dist/d3.min.js']
 
     // List of classnames that will be attached to your plugin upon mounting
-    // className: 'plugin-lhpane plugin-mobile-fullscreen',
+    className: 'plugin-lhpane plugin-mobile-fullscreen',
 
     // If you want to apply different set of classes on mobile devices
     // classNameMobile: 'this-is-other-class',
 
     // Forces all other window panes with the same value to be closed
     // Allowed: 'lhpane', 'rhpane' and 'all'
-    // exclusive: 'lhpane',
+    exclusive: 'lhpane',
 
     // The place in page, where your plugin element will be mounted
     // to the page. By default all the plugins are attached to
